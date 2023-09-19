5fc4d39d244df9fba29badec4f1df7c08ef48754
elverdavid <elverdavid0839@gmail.com>
2023-09-19 09:47:52 -0500

    improved navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import SectionCategories from "@/LandingPage/components/SectionCategories";
 import Hero from "@/LandingPage/components/Hero";
-import Navbar from "@/LandingPage/components/Navbar";
+
 import SectionRecipesLatest from "@/LandingPage/components/SectionRecipesLatest";
 
 const HomePage = () => {
   return (
     <main>
-      <Navbar />
       <Hero />
       <SectionCategories />
       <SectionRecipesLatest />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,27 +2,27 @@ import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				light: "#FFF9EA",
-				gold: "#FFAC4B",
-			},
-			fontFamily: {
-				kanit: ["var(--kanit)"],
-				rubik: ["var(--rubik)"],
-			},
-			gridTemplateColumns: {
-        'auto-fit': 'repeat(auto-fit, minmax(200px, 1fr))'
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: "#FFF9EA",
+        gold: "#FFAC4B",
       },
-		},
-	},
-	plugins: [nextui()],
+      fontFamily: {
+        kanit: ["var(--kanit)"],
+        rubik: ["var(--rubik)"],
+      },
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(200px, 1fr))",
+      },
+    },
+  },
+  plugins: [nextui()],
 };
 export default config;
