e8edda96ed964c43482d4965529a15eca03e2d1c
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-10-04 11:42:25 -0500

    fix: imrpove the props of ImageProps

--- src/app/interfaces/ImageProps.interface.ts
+++ src/app/interfaces/ImageProps.interface.ts
@@ -1,7 +1,8 @@
-export interface ImageProps {
+import { type ImageProps as ImagePropsNextJS } from 'next/image'
+
+// extends of the interface ImagePropsNextJS to avoid add manually each prop
+export interface ImageProps extends Omit<ImagePropsNextJS, 'src'> {
   url: string
   name: string
-  width: number
-  height: number
   customStyles?: string
 }
