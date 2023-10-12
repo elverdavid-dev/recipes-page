5c8ce13d7562a73b4a22912dadff7f2b743cf6d5
Elver David <elverdavid0839@gmail.com>
2023-10-12 13:48:30 -0500

    mejoras en el diseño de la card de country

--- src/app/LandingPage/components/SectionCountry.tsx
+++ src/app/LandingPage/components/SectionCountry.tsx
@@ -12,14 +12,19 @@ const SectionCountry = async () => {
       <section className="grid lg:grid-cols-3 justify-center gap-5">
         {countrys?.data.map(({ _id, image, name }) => (
           <ShowRecipesOneCountry key={_id} id={_id}>
-            <Image
-              src={image}
-              alt={name}
-              width={400}
-              height={300}
-              className="mx-auto"
-            />
-            <span className="text-lg text-slate-800 font-nunito">{name} </span>
+            <div className="relative group">
+              <Image
+                src={image}
+                alt={name}
+                width={400}
+                height={300}
+                className="transition-transform group-hover:scale-105"
+              />
+              <span className="text-2xl text-white font-kanit absolute bottom-2 left-5 z-10">
+                {name}
+              </span>
+              <div className="absolute inset-0 bg-black opacity-50 " />
+            </div>
           </ShowRecipesOneCountry>
         ))}
       </section>
