0ebef0957afdad75d75ca760e05f216254b5f371
Elver David <elverdavid0839@gmail.com>
2023-12-03 15:05:13 -0500

    imagenes de carusel mas grandes en pantalla pequeña

--- src/app/LandingPage/components/Carusel.tsx
+++ src/app/LandingPage/components/Carusel.tsx
@@ -31,7 +31,7 @@ const Carusel = ({ categories }: { categories: Categories[] | undefined }) => {
 									alt={name}
 									width={264}
 									height={188}
-									className="hover:scale-[1.03] transition-all rounded-md w-[192px] h-[137px] md:w-[264px] md:h-[188px]"
+									className="hover:scale-[1.03] transition-all rounded-md w-[230px] h-[150px] md:w-[264px] md:h-[188px]"
 								/>
 								<span className="text-xl text-slate-950 font-nunito block py-2 group-hover:text-gold transition-all ">
 									{name}{' '}
--- src/app/lib/ResponsiveCaruselOptions.tsx
+++ src/app/lib/ResponsiveCaruselOptions.tsx
@@ -1,8 +1,8 @@
 export const ResponsiveOptions = {
 	// cuando el ancho de la ventana sea  <= 300px
 	300: {
-		slidesPerView: 1.90,
-		spaceBetween: 10,
+		slidesPerView: 1.55,
+		spaceBetween: 5,
 	},
 	480: {
 		slidesPerView: 2.2,
